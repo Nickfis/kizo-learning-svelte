904f04fab3f2edf7f3340d52b5a17d6790890d19
Nik <niklas@kizo-agency.com>
2024-01-07 18:24:40 +0100

    Updated Header

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+	import logo from '$assets/logo.png';
+</script>
+
 <footer class="bg-white px-6 py-4 shadow-inner">
 	<div class="flex justify-between items-center">
-		<span class="text-sm text-gray-700">KIZO LOGO</span>
+		<img src={logo} alt="" />
 		<div class="flex items-center space-x-4">
 			<a href="/imprint" class="text-gray-700 hover:text-pink-600 text-sm">Imprint</a>
 			<a href="/data-privacy" class="text-gray-700 hover:text-pink-600 text-sm">Data Privacy</a>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,10 +2,16 @@
 	import logo from '$assets/logo.png';
 </script>
 
-<nav class="flex justify-between items-center bg-white px-6 py-4 shadow-md">
-	<div class="font-bold text-xl"><img src="" alt="" /></div>
-	<div class="flex items-center space-x-4">
-		<a href="/promotions" class="text-gray-700 hover:text-pink-600">Promotions</a>
-		<a href="/projects" class="text-gray-700 hover:text-pink-600">Projects</a>
-	</div>
+<nav class="flex justify-between items-center bg-white px-12 py-4 shadow">
+	<a href="/" class="block">
+		<img src={logo} alt="Go to home" class="h-10" />
+	</a>
+	<ul class="flex items-center mr-8">
+		<li>
+			<a href="/promotions" class="text-gray-900 hover:text-pink-600 text-xl">Promotions</a>
+		</li>
+		<li class="ml-14">
+			<a href="/projects" class="text-gray-900 hover:text-pink-600 text-xl">Projects</a>
+		</li>
+	</ul>
 </nav>
