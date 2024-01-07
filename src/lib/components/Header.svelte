5db473f74bc1e99bc059874320d3ab8a7b0f38c8
Nik <niklas@kizo-agency.com>
2024-01-07 18:33:55 +0100

    Header responsiveness

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,16 +2,18 @@
 	import logo from '$assets/logo.png';
 </script>
 
-<nav class="flex justify-between items-center bg-white px-12 py-4 shadow">
+<nav class="flex justify-between items-center bg-white px-7 sm:px-12 py-4 shadow">
 	<a href="/" class="block">
-		<img src={logo} alt="Go to home" class="h-10" />
+		<img src={logo} alt="Go to home" class="h-8 sm:h-10" />
 	</a>
-	<ul class="flex items-center mr-8">
+	<ul class="flex items-center sm:mr-8">
 		<li>
-			<a href="/promotions" class="text-gray-900 hover:text-pink-600 text-xl">Promotions</a>
+			<a href="/promotions" class="text-gray-900 hover:text-pink-600 sm:text-xl text-lg"
+				>Promotions</a
+			>
 		</li>
-		<li class="ml-14">
-			<a href="/projects" class="text-gray-900 hover:text-pink-600 text-xl">Projects</a>
+		<li class="ml-4 sm:ml-14">
+			<a href="/projects" class="text-gray-900 hover:text-pink-600 sm:text-xl text-lg">Projects</a>
 		</li>
 	</ul>
 </nav>
